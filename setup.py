153fc3247ebba143a720818d4bb1e18fd1b995ef
hmatuschek <hmatuschek@bc97c740-7742-0410-9261-53114e3671a7>
2008-01-05 17:43:34 +0000

    * fixed install of syntax-files

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 
 from distutils.core import setup
+import os.path
+import glob 
+
+syntax_files = glob.glob(os.path.join("syntax","*.xml"))
 
 desc = """PyGTKCodeBuffer - Lightweight syntax-highlighting for PyGTK's TextView-widget."""
 
@@ -11,13 +15,13 @@ standard library. No Gnome nor Scintilla libraries are needed so it should run
 perfectly under all platforms supported by PyGTK!"""
 
 setup ( name = 'PyGTKCodeBuffer',
-        version = '0.2.0',
+        version = '0.3.0',
         description = desc,
         long_description = long_desc,
         author = 'Hannes Matuschek',
         author_email = 'hmatuschek@gmail.com',
         url = 'http://pygtkcodebuffer.googlecode.com',
-        download_url = 'http://pygtkcodebuffer.googlecode.com/files/PyGTKCodeBuffer-0.2.0.tar.bz2',
+        download_url = 'http://pygtkcodebuffer.googlecode.com/files/PyGTKCodeBuffer-0.3.0.tar.bz2',
         
         classifiers = ['Development Status :: 4 - Beta',
                        'Environment :: X11 Applications :: GTK',
@@ -31,6 +35,5 @@ setup ( name = 'PyGTKCodeBuffer',
 
         py_modules = ['gtkcodebuffer'],
         
-        data_files = [('share/pygtkcodebuffer/syntax',   
-                        ['syntax/python.xml', 'syntax/cpp.xml'])]
+        data_files = [('share/pygtkcodebuffer/syntax', syntax_files)]
       )                
